4cc2a3fabcb8b2686149957497066f1547a992b6
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-09-07 23:03:02 +0900

    Optimize for Qt5

--- qdarktheme/designer_supporter/__main__.py
+++ qdarktheme/designer_supporter/__main__.py
@@ -2,9 +2,14 @@ import sys
 
 import qdarktheme
 from qdarktheme.designer_supporter.mainwindow import MainDialog
+from qdarktheme.qtpy.QtCore import Qt
 from qdarktheme.qtpy.QtWidgets import QApplication
 
 app = QApplication(sys.argv)
+# Fix the svg icon display becoming low quality in Qt5.
+# PyQt6 doesn't have this attribute.
+if hasattr(Qt.ApplicationAttribute, "AA_UseHighDpiPixmaps"):
+    app.setAttribute(Qt.ApplicationAttribute.AA_UseHighDpiPixmaps)
 main_win = MainDialog()
 main_win.show()
 app.setStyleSheet(qdarktheme.load_stylesheet())
