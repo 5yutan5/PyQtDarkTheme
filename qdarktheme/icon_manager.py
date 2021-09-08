96484a45df2bd8fd597b06702646f482cf2fc8d0
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-09-08 18:42:25 +0900

    Change the svg folder from name example to app

--- qdarktheme/icon_manager.py
+++ qdarktheme/icon_manager.py
@@ -4,19 +4,19 @@ from qdarktheme.qtpy.QtGui import QIcon
 
 
 def get_icon(icon_name: str) -> QIcon:
-    """Creates an icon from `qdarktheme/svg/example`. See below folder(`qdarktheme/svg/example`) for details on
-    available icons.
+    """Creates an icon from svg file in `qdarktheme/svg/app`. See below folder(`qdarktheme/svg/app`)
+    for details on available icons.
 
     Parameters
     ----------
     icon_name : str
-        Available icon name: "clear", "circle", "copy_all", "dark_mode", "favorite_border", "flip_to_front",
-        "folder_open", "home", "light_mode", "palette", "settings",
+        Available icon name: "clear", "circle", "copy_all", "favorite_border", "flip_to_front",
+        "folder_open", "home", "palette", "settings"
 
     Returns
     -------
     QIcon
         QIcon object with the given icon name.
     """
-    with resources.path("qdarktheme.svg.example", f"{icon_name}_24dp.svg") as icon_path:
+    with resources.path("qdarktheme.svg.app", f"{icon_name}_24dp.svg") as icon_path:
         return QIcon(str(icon_path))
