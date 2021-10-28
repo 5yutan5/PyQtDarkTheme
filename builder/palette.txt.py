fefcaa7379c819ce9729758d4b9532d31ce19c51
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-10-29 01:18:06 +0900

    Add additional palette

--- builder/palette.txt.py
+++ builder/palette.txt.py
@@ -7,7 +7,7 @@ _palette.setColor(QPalette.ColorRole.WindowText, QColor("$text"))
 _palette.setColor(QPalette.ColorRole.Button, QColor("$base"))
 _palette.setColor(QPalette.ColorRole.Text, QColor("$text"))
 _palette.setColor(QPalette.ColorRole.ButtonText, QColor("$highlight"))
-_palette.setColor(QPalette.ColorRole.Base, QColor("$input-line"))
+_palette.setColor(QPalette.ColorRole.Base, QColor("$base"))
 _palette.setColor(QPalette.ColorRole.Window, QColor("$base"))
 _palette.setColor(QPalette.ColorRole.Highlight, QColor("$highlight"))
 _palette.setColor(QPalette.ColorRole.HighlightedText, QColor("$highlight-text"))
@@ -16,8 +16,18 @@ _palette.setColor(QPalette.ColorRole.AlternateBase, QColor("$itemview-alternate"
 _palette.setColor(QPalette.ColorRole.ToolTipBase, QColor("$popup"))
 _palette.setColor(QPalette.ColorRole.ToolTipText, QColor("$popup-text"))
 _palette.setColor(QPalette.ColorRole.LinkVisited, QColor("$link-visited"))
+_palette.setColor(QPalette.ColorRole.Foreground, QColor("$text"))
 if hasattr(QPalette.ColorRole, "PlaceholderText"):
     _palette.setColor(QPalette.ColorRole.PlaceholderText, QColor("$placeholder-text"))
+_palette.setColor(QPalette.ColorRole.BrightText, QColor("red"))
+_palette.setColor(QPalette.ColorRole.ToolTipText, QColor("$popup"))
+_palette.setColor(QPalette.ColorRole.ToolTipBase, QColor("$popup-text"))
+
+_palette.setColor(QPalette.ColorRole.Light, QColor("$border"))
+_palette.setColor(QPalette.ColorRole.Midlight, QColor("$border"))
+_palette.setColor(QPalette.ColorRole.Dark, QColor("$text"))
+_palette.setColor(QPalette.ColorRole.Mid, QColor("$border"))
+_palette.setColor(QPalette.ColorRole.Shadow, QColor("$border"))
 
 # disabled
 _palette.setColor(QPalette.ColorGroup.Disabled, QPalette.ColorRole.WindowText, QColor("$text-disabled"))
