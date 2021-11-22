b377d530ec0d0bb19a38fba42d5d51265ef4e0dc
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-11-22 20:49:06 +0900

    Make build resources program automatic

--- builder/__main__.py
+++ builder/__main__.py
@@ -5,24 +5,26 @@
 
 from __future__ import annotations
 
+import importlib.util
 import json
+import pprint
 import re
 import shutil
 import subprocess  # nosec
 from dataclasses import dataclass
+from filecmp import cmpfiles
 from importlib import resources
 from pathlib import Path
+from sys import executable as python
 from tempfile import NamedTemporaryFile, TemporaryDirectory
 from xml.etree import ElementTree as ET  # nosec
 
 import click
-from defusedxml import defuse_stdlib
 
 from builder.color import RGBA
-from qdarktheme.util import multireplace
+from qdarktheme.util import get_project_root_path, multireplace
 
-# Prevent Vulnerability of xml
-defuse_stdlib()
+DIST_DIR_PATH = get_project_root_path() / "dist"
 
 
 @dataclass(unsafe_hash=True, frozen=True)
@@ -62,7 +64,7 @@ def parse_url(stylesheet: str) -> set[Url]:
     return urls
 
 
-def build_svg_file(urls: set[Url], colors: dict[str, RGBA], output_folder_path: Path) -> None:
+def build_svg_file(urls: set[Url], colors: dict[str, RGBA], output_dir_path: Path) -> None:
     svg_codes: dict[str, str] = {}  # {file name: svg code}
     for content in resources.contents("builder.svg"):
         if ".svg" not in content:  # Only svg file
@@ -79,105 +81,115 @@ def build_svg_file(urls: set[Url], colors: dict[str, RGBA], output_folder_path: 
         # Change color and rotate. See https://stackoverflow.com/a/15139069/13452582
         new_contents = f'{to_svg_color_format(rgba)} transform="rotate({url.rotate}, 12, 12)"'
         svg_code_converted = svg_codes[url.icon].replace('fill="#FFFFFF"', new_contents)
-
-        with (output_folder_path / url.file_name).open("w") as f:
+        with (output_dir_path / url.file_name).open("w") as f:
             f.write(svg_code_converted)
 
 
-def build_palette_file(colors: dict[str, RGBA], output_folder_path: Path) -> None:
+def build_palette_file(colors: dict[str, RGBA], output_dir_path: Path) -> None:
     def to_arg_text(rgba: RGBA) -> str:
         r, g, b, a = rgba
         return f"{r}, {g}, {b}, {a*255}"
 
     replacements = {f'"${color_id}"': to_arg_text(rgba) for color_id, rgba in colors.items()}
     palette_text = resources.read_text("builder", "palette.txt.py")
-    with (output_folder_path / "palette.py").open("w") as f:
+    with (output_dir_path / "palette.py").open("w") as f:
         f.write(multireplace(palette_text, replacements))
+
+
+def compare_all_files(output_dir: Path, temp_dir: Path) -> list[str]:
+    target_files = set()
+    for file in temp_dir.glob("**/*"):
+        if not file.is_file():
+            continue
+        target_files.add(str(file).replace(str(temp_dir), "")[1:])
+    _, mismatch, err = cmpfiles(output_dir, temp_dir, target_files)
+    files_changed = mismatch + err
+    return [str(Path(output_dir).relative_to(Path.cwd()) / file) for file in files_changed]
+
+
+def generate_qt_resource_file(svg_dir_path: Path, output_dir_path: Path, theme: str) -> None:
+    main_tag = ET.Element("RCC", {"version": "1.0"})
+    qt_resource_tag = ET.SubElement(main_tag, "qresource", {"prefix": f"qdarktheme/dist/{theme}"})
+
+    for file in svg_dir_path.iterdir():
+        file_tag = ET.SubElement(qt_resource_tag, "file")
+        file_tag.text = f"{svg_dir_path.name}/{file.name}"
+
+    with NamedTemporaryFile(suffix=".qrc", dir=str(output_dir_path)) as f:
+        qrc_file_path = output_dir_path / f.name
+        ET.ElementTree(main_tag).write(str(qrc_file_path), "utf-8")
+
+        py_resource_file_path = output_dir_path / "rc_icons.py"
+        subprocess.check_call(["pyside6-rcc", qrc_file_path, "-o", py_resource_file_path])  # nosec
+    py_resource_text_converted = py_resource_file_path.read_text().replace("PySide6", "qdarktheme.qtpy")
+    py_resource_file_path.write_text(py_resource_text_converted)
+
+
+def build_theme_resources(root_path: Path, theme_file_path: Path) -> None:
+    theme = theme_file_path.stem
+    output_dir_path = root_path / theme
+    output_dir_path.mkdir()
+    (output_dir_path / "__init__.py").touch()
+
+    hex_colors = json.loads(theme_file_path.read_bytes())
+    rgba_colors = {color_id: RGBA.from_hex(color_hex) for color_id, color_hex in hex_colors.items()}
+
+    # Build svg
+    svg_dir_path = output_dir_path / "svg"
+    svg_dir_path.mkdir()
+    build_svg_file(urls, rgba_colors, svg_dir_path)
+
+    # Build palette file
+    build_palette_file(rgba_colors, output_dir_path)
+
+    # Build template stylesheet
+    url_replacements = {url.match_text: f"url(${{path}}/dist/{theme}/svg/{url.file_name})" for url in urls}
+    colors_converted = {f"${color_id}": str(rgba) for color_id, rgba in rgba_colors.items()}
+    template_stylesheet = multireplace(stylesheet, {**url_replacements, **colors_converted})
+    with (output_dir_path / "stylesheet.py").open("w") as f:
+        f.write(f'STYLE_SHEET = """\n{template_stylesheet}\n"""\n')
+
+    # Generate qt resource file
+    dist_dir_rc_icons_path = DIST_DIR_PATH / theme / "rc_icons.py"
+    if compare_all_files(DIST_DIR_PATH / theme, output_dir_path):
+        generate_qt_resource_file(svg_dir_path, output_dir_path, theme)
+    elif not dist_dir_rc_icons_path.exists():
+        generate_qt_resource_file(svg_dir_path, output_dir_path, theme)
+    else:
+        shutil.copy(dist_dir_rc_icons_path, output_dir_path / "rc_icons.py")
 
 
 if __name__ == "__main__":
     stylesheet = resources.read_text("builder", "base.qss")
     stylesheet = remove_comment(stylesheet)
     urls = parse_url(stylesheet)
-    is_installed = False
+    is_installed_pyside6 = False
 
     # Install PySide6
-    command = "poetry run pip install pyside6"
-    proc = subprocess.run(command.split(), capture_output=True)  # nosec
-    if proc.returncode == 1:
-        click.secho(proc.stdout.decode() + proc.stderr.decode(), err=True, fg="red")
-        raise RuntimeError(f"Failed to run '{command}'")
-    click.echo(proc.stdout.decode())
-    click.secho(proc.stderr.decode(), err=True, fg="yellow")
-    if "Requirement already satisfied" in proc.stdout.decode():
-        is_installed = True
+    if importlib.util.find_spec("PySide6"):
+        is_installed_pyside6 = True
+    else:
+        subprocess.check_call([python, "-m", "pip", "install", "pyside6"])  # nosec
 
-    # Setup temp dir
     with TemporaryDirectory() as temp_dir:
-        temp_dir_path = Path(str(temp_dir))
-        (temp_dir_path / "__init__.py").touch()
+        temp_dir_path = Path(temp_dir)
+        shutil.copy(Path(__file__).parent / "__init__.py", temp_dir_path / "__init__.py")
 
-        for contents in resources.contents("builder.theme"):
-            if ".json" not in contents or "validate.json" in contents:
-                continue
-            with resources.open_binary("builder.theme", contents) as f:
-                colors = {color_id: RGBA.from_hex(color_hex) for color_id, color_hex in json.load(f).items()}
-                theme_name = contents.replace(".json", "")
+        for path in Path(__file__).parent.glob("theme/*.json"):
+            if "validate.json" != path.name:
+                build_theme_resources(temp_dir_path, theme_file_path=path)
+        # Refresh dist dir
+        changed_files = compare_all_files(DIST_DIR_PATH, temp_dir_path)
+        if changed_files:
+            shutil.rmtree(DIST_DIR_PATH, ignore_errors=True)
+            shutil.copytree(temp_dir, DIST_DIR_PATH)
 
-            # Setup output folder
-            output_folder_path = temp_dir_path / theme_name
-            output_folder_path.mkdir()
-            (output_folder_path / "__init__.py").touch()
-
-            # Build svg
-            svg_folder_path = output_folder_path / "svg"
-            svg_folder_path.mkdir()
-            build_svg_file(urls, colors, svg_folder_path)
-
-            # Build palette file
-            build_palette_file(colors, output_folder_path)
-
-            # Build template stylesheet
-            url_replacements = {
-                url.match_text: f"url(${{path}}/dist/{theme_name}/svg/{url.file_name})" for url in urls
-            }
-            colors = {f"${color_id}": str(rgba) for color_id, rgba in colors.items()}
-            template_stylesheet = multireplace(stylesheet, {**url_replacements, **colors})
-
-            with (output_folder_path / "stylesheet.py").open("w") as f:
-                py_text = f'STYLE_SHEET = """\n{template_stylesheet}\n"""\n'
-                f.write(py_text)
-
-            # generate resource file as temporary
-            main_tag = ET.Element("RCC", {"version": "1.0"})
-            qt_resource_tag = ET.SubElement(main_tag, "qresource", {"prefix": f"qdarktheme/dist/{theme_name}"})
-
-            for file in svg_folder_path.iterdir():
-                file_tag = ET.SubElement(qt_resource_tag, "file")
-                file_tag.text = f"{svg_folder_path.name}/{file.name}"
-
-            with NamedTemporaryFile(suffix=".qrc", dir=str(output_folder_path)) as f:
-                qrc_file_path = output_folder_path / f.name
-                ET.ElementTree(main_tag).write(str(qrc_file_path), "utf-8")
-
-                py_resource_file_path = output_folder_path / "rc_icons.py"
-                subprocess.run(f"poetry run pyside6-rcc {qrc_file_path} -o {py_resource_file_path}".split())  # nosec
-            py_resource_text_converted = py_resource_file_path.read_text().replace("PySide6", "qdarktheme.qtpy")
-            py_resource_file_path.write_text(py_resource_text_converted)
-            subprocess.run(f"poetry run black {py_resource_file_path} --line-length=119".split())  # nosec
-
-        # Refresh dist folder
-        dist_folder_path = Path.cwd() / "qdarktheme" / "dist"
-        shutil.rmtree(str(dist_folder_path), ignore_errors=True)
-        shutil.copytree(temp_dir, dist_folder_path)
-        (dist_folder_path / "__init__.py").touch()
-
-    click.echo(
-        click.style("\nBuild finished!\n", fg="green")
-        + "changed some content in "
-        + click.style(dist_folder_path.relative_to(Path.cwd()), bold=True)
-        + "\n"
-    )
-    if not is_installed:
-        subprocess.run("poetry run pip uninstall -y pyside6 shiboken6".split())  # nosec
-        click.echo("Install and uninstall PySide6")
+    click.secho("Build finished!", fg="green")
+    if changed_files:
+        click.echo("Changed contents: " + click.style("[", fg="yellow"))
+        click.secho(pprint.pformat(changed_files).replace("[", " ").replace("]", ""), bold=True)
+        click.secho("]", fg="yellow")
+    else:
+        click.echo("There is no change")
+    if not is_installed_pyside6:
+        subprocess.check_call([python, "-m", "pip", "uninstall", "-y", "pyside6", "shiboken6"])  # nosec
