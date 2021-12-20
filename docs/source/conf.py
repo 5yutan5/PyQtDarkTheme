5e7ca84a67511d222a462b95a296d1bc26fb86c7
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-12-21 01:00:07 +0900

    Update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,7 @@ import sys
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-path = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(path, "..", ".."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
