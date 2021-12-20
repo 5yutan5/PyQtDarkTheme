f8808cfaf5e25760dfff8b4f3cf5c7f322272de7
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2021-12-21 00:47:29 +0900

    Fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,21 +7,21 @@
 
 # -- Path setup --------------------------------------------------------------
 
+import os
 import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-from pathlib import Path
 
-sys.path.insert(0, str(Path(".").parent))
+sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 
 
 # -- Project information -----------------------------------------------------
 project = "PyQtDarkTheme"
-copyright = "2021, PyQtDarkTheme"
-author = "PyQtDarkTheme"
+copyright = "2021, Yunosuke Ohsugi"
+author = "Yunosuke Ohsugi"
 
 # -- General configuration ---------------------------------------------------
 
