f51cf3b8d8b4a4a0c922bf1c9b19612a8c80aeb8
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2022-01-02 02:39:43 +0900

    Fix

--- .github/scripts/test_freezing_pkg.py
+++ .github/scripts/test_freezing_pkg.py
@@ -5,11 +5,20 @@ from pathlib import Path
 import click
 import PyInstaller.__main__ as pyinstaller  # type: ignore
 
+IGNORE_MESSAGES = (
+    "Unable to create basic Accelerated OpenGL renderer.",
+    "Core Image is now using the software OpenGL renderer. This will be slow.",
+)
+
 
 class SvgFileNotFoundError(FileNotFoundError):
     """Error raise if cannot open svg file."""
 
     pass
+
+
+def _print_successfull_message() -> None:
+    click.secho("Build finished successfully!", fg="green")
 
 
 def _create_app_with_pyinstaller(target: Path) -> Path:
@@ -21,29 +30,42 @@ def _create_app_with_pyinstaller(target: Path) -> Path:
     return app_path
 
 
-def _test_pyinstaller() -> None:
+def _test_pyinstaller() -> str:
     click.echo("================================")
     click.echo("Building app with PyInstaller...")
     click.echo("================================")
     demo_app_src_path = Path(__file__).parent / "demo_app.py"
     demo_app_path = _create_app_with_pyinstaller(demo_app_src_path)
-    result = subprocess.check_output(demo_app_path, stderr=subprocess.STDOUT, encoding="utf-8")
-    if result == "":
+    return subprocess.check_output([str(demo_app_path)], stderr=subprocess.STDOUT, encoding="utf-8")
+
+
+def _check_output(output: str) -> None:
+    if output == "":
         click.echo("There is no output from demo app")
-        click.secho("Build finished successfully!", fg="green")
+        _print_successfull_message()
         return
     else:
-        click.echo("Demo app output:")
         click.echo("----------------")
-        click.echo(result)
+        click.echo("Demo app outputs")
+        click.echo("----------------")
+        click.echo(output)
 
-    if "qt.svg: Cannot open file" in result:
+    if "qt.svg: Cannot open file" in output:
         raise SvgFileNotFoundError("QtSvg module cannot open svg files, because: No such file or directory")
+    for ignore_image in IGNORE_MESSAGES:
+        if ignore_image in output:
+            click.echo(f"Ignore: {ignore_image}")
+            output = output.replace(ignore_image, "")
+    result = output.replace("\n", "")
+    if result == "":
+        _print_successfull_message()
     raise RuntimeError("There is unexpected output")
 
 
 def _main() -> None:
-    _test_pyinstaller()
+    click.echo("Build start")
+    output = _test_pyinstaller()
+    _check_output(output)
 
 
 if __name__ == "__main__":
