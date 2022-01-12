291893318323123df72380aa0850df3961be7e3b
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2022-01-12 14:22:33 +0900

    Refactor test for qt actions

--- tests/test_widget_gallery.py
+++ tests/test_widget_gallery.py
@@ -13,19 +13,19 @@ def widget_gallery(qtbot) -> WidgetGallery:
     return widget_gallery
 
 
-def test_change_page(widget_gallery: WidgetGallery) -> None:
-    """Ensure the change page UX works."""
-    for action in widget_gallery._ui.actions_page:
-        action.triggered.emit()
+def test_actions(widget_gallery: WidgetGallery, monkeypatch: pytest.MonkeyPatch) -> None:
+    """Ensure the actions work correctly."""
+    from qdarktheme.qtpy.QtWidgets import QMessageBox
 
+    monkeypatch.setattr(QMessageBox, "question", lambda a, b, c: (a, b, c))
+    monkeypatch.setattr(QMessageBox, "information", lambda a, b, c: (a, b, c))
+    monkeypatch.setattr(QMessageBox, "warning", lambda a, b, c: (a, b, c))
+    monkeypatch.setattr(QMessageBox, "critical", lambda a, b, c: (a, b, c))
 
-def test_toggle_state(widget_gallery: WidgetGallery) -> None:
-    """Ensure the toggle state UX works."""
-    for action in [widget_gallery._ui.action_enable, widget_gallery._ui.action_disable]:
-        action.triggered.emit()
-
-
-def test_change_theme(widget_gallery: WidgetGallery) -> None:
-    """Ensure the change theme UX works."""
-    for action in widget_gallery._ui.actions_theme:
+    actions = [widget_gallery._ui.action_enable, widget_gallery._ui.action_disable]
+    actions += widget_gallery._ui.actions_page
+    actions += widget_gallery._ui.actions_theme
+    actions += widget_gallery._ui.actions_message_box
+    actions += widget_gallery._ui.actions_message_box
+    for action in actions:
         action.triggered.emit()
