85a1190eb6504c82dc4fb1d492d5fc60c6886bcd
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2022-01-20 16:45:38 +0900

    Fix toolbar extra button

--- qdarktheme/themes/light/stylesheet.py
+++ qdarktheme/themes/light/stylesheet.py
@@ -185,6 +185,7 @@ QToolBar > QToolButton:checked {
 }
 QToolBar > QToolButton#qt_toolbar_ext_button {
     image: url(${path}/themes/light/svg/double_arrow__icon-foreground.svg);
+    $env_patch{"os": "Windows", "value": "padding: 0;"};
 }
 QToolBar > QToolButton#qt_toolbar_ext_button:disabled {
     image: url(${path}/themes/light/svg/double_arrow__icon-foreground-disabled.svg);
