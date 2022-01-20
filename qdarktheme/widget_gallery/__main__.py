723c4be1de59109bef70ee0561080534b9006121
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2022-01-20 17:03:05 +0900

    Remove a test line

--- qdarktheme/widget_gallery/__main__.py
+++ qdarktheme/widget_gallery/__main__.py
@@ -13,6 +13,5 @@ if __name__ == "__main__":
     win = WidgetGallery()
     win.menuBar().setNativeMenuBar(False)
     app.setStyleSheet(qdarktheme.load_stylesheet())
-    app.setStyle("Windows")
     win.show()
     app.exec()
