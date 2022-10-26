805032da46213901649e3c31235ebda2ea899cb4
Yunosuke Ohsugi <63651161+5yutan5@users.noreply.github.com>
2022-10-26 05:23:13 +0000

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 
 # -- Project information -----------------------------------------------------
-project = "pyqtdarktheme"
+project = "PyQtDarkTheme"
 copyright = f"2021-2022, {package_metadata(project)['author']}"
 author = package_metadata(project)["author"]
 
