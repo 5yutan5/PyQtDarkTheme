15a6aa5d64dbdbad4c79c814e940f8723316554a
5yutan5 <63651161+5yutan5@users.noreply.github.com>
2022-12-12 15:23:32 +0900

    Fix attr error

--- qdarktheme/qtpy/qt_version.py
+++ qdarktheme/qtpy/qt_version.py
@@ -7,7 +7,7 @@ __version__: str | None = None
 if QT_API == "PySide6":
     from PySide6.QtCore import qVersion  # type: ignore
 
-    __version__ = qVersion().decode("utf-8")
+    __version__ = qVersion()
 elif QT_API == "PyQt6":
     from PyQt6.QtCore import qVersion  # type: ignore
 
@@ -19,4 +19,4 @@ elif QT_API == "PyQt5":
 elif QT_API == "PySide2":
     from PySide2.QtCore import qVersion  # type: ignore
 
-    __version__ = qVersion().decode("utf-8")
+    __version__ = qVersion()
